086a62a898ca549b26328306a5690e18702ab86b
Bruno Roemers <bruno@roemers.io>
2020-04-11 17:21:33 +0200

    Configure Webpack for images

--- TODO.md
+++ TODO.md
@@ -1,8 +1,10 @@
 # TODO's
+- mobile
+- images
+- single page
+- list page dynamic
 - CTA hover state
 - CTA mail
-- single page
-- mobile
 - inline critical css
   - https://vuejsdevelopers.com/2017/07/24/critical-css-webpack/
   - https://github.com/anthonygore/html-critical-webpack-plugin
--- webpack.common.js
+++ webpack.common.js
@@ -1,13 +1,22 @@
 const path = require('path')
+const glob = require('glob')
 const AssetsPlugin = require('assets-webpack-plugin')
+const ManifestPlugin = require('webpack-manifest-plugin')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 
+const mainjs = path.join(__dirname, 'src/js/main.js')
+const maincss = path.join(__dirname, 'src/css/main.css')
+
 module.exports = {
-  entry: {
-    main: [
-      path.join(__dirname, 'src/js/main.js'),
-      path.join(__dirname, 'src/css/main.css'),
-    ],
+  entry: () => {
+    const images = glob.sync(
+      path.join(__dirname, 'src/images/**/*'),
+      { nodir: true }
+    )
+
+    return {
+      main: [mainjs, maincss, ...images]
+    }
   },
   output: {
     path: path.join(__dirname, 'public'),
@@ -23,18 +32,49 @@ module.exports = {
         },
       },
       {
+        test: /\.(png|jpg|gif)$/,
+        loader: 'file-loader',
+        options: {
+          name: 'images/[name].[ext]',
+        },
+      },
+      {
         test: /\.svg$/i,
-        loader: 'svg-url-loader',
+        use: [
+          {
+            loader: 'svg-url-loader',
+            options: {
+              limit: 2048, // 2kb
+              fallback: 'file-loader',
+
+              // options for fallback
+              name: 'images/[name].[ext]',
+            },
+          },
+        ],
       },
     ],
   },
   plugins: [
+    // data/webpack.json (entrypoints)
     new AssetsPlugin({
       filename: "webpack.json",
       path: path.join(__dirname, 'data'),
       prettyPrint: true,
       entrypoints: true,
     }),
+
+    // data/webpack-images.json
+    new ManifestPlugin({
+      fileName: path.join(__dirname, 'data/webpack-images.json'),
+      filter: ({path}) => /^images\/.*/.test(path),
+      generate: (seed, files, entry) => files.reduce((manifest, { name, path }) => ({
+        ...manifest,
+        [name]: [...(manifest[name] || []), path],
+      }), seed),
+    }),
+
+    // clean old output products
     new CleanWebpackPlugin(),
   ],
 }
