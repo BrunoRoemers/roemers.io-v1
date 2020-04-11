acec4e7f47f275f0c386af95303d52f28c91c691
Bruno Roemers <bruno@roemers.io>
2020-04-11 23:05:09 +0200

    Style images (large screens)

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,10 @@ module.exports = {
         'cta-on-white': '#ea1020',
         'cta-on-black': '#ef626c',
       },
+      boxShadow: {
+        'on-white': '1px 1px 12px rgba(34, 37, 45, 0.2)',
+        'on-black': '-1px 1px 12px rgba(0, 0, 0, 0.25)',
+      },
       spacing: {
         '14': '3.5rem',
         '22': '5.5rem',
