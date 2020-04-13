f3a2904d03afbd1deb745bd8f2cfd47d9b5776d0
Bruno Roemers <bruno@roemers.io>
2020-04-13 15:54:59 +0200

    Add temporary notice for inactive links

--- TODO.md
+++ TODO.md
@@ -1,13 +1,9 @@
 # TODO's
-- mobile
-- images
 - single page
 - list page dynamic
 - CTA hover state
 - CTA mail
+- optimize images
 - inline critical css
   - https://vuejsdevelopers.com/2017/07/24/critical-css-webpack/
   - https://github.com/anthonygore/html-critical-webpack-plugin
-- Improve SVG inlining
-  - https://discourse.gohugo.io/t/embedding-inline-svg-in-content-markdown/7511
-  - ManifestPlugin -> icons.json?
--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,25 @@
 // fonts
 require('typeface-quicksand')
 require('typeface-raleway')
+
+// TEMP: alert user when inactive link/button is clicked
+window.addEventListener('DOMContentLoaded', (event) => {
+    window.addEventListener('click', (event) => {
+        const clickTarget = event.target || event.srcElement
+        const inactiveLink = closestWith('data-inactive', clickTarget)
+
+        if (inactiveLink === null) return
+
+        alert(`This link is not yet active. I'm working on it :)`)
+    })
+})
+
+function closestWith(dataLabel, el) {
+    if (el === null) return null
+
+    // element has dataLabel applied
+    if (el.getAttribute(dataLabel) !== null) return el
+
+    // drill down one level
+    return closestWith(dataLabel, el.parentElement)
+}
